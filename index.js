5edca34974a47d0248292eacc4f71c1ecbfcc488
Tom Klinovsky <tom@kiwinet.co.il>
2018-03-29 14:53:52 +0300

    added the original code and tests (this was a project module in another project)

---
+++ index.js
@@ -0,0 +1,59 @@
+module.exports = trackerFactory;
+
+function getMissingElements(last_array, cur_array, compare) {
+  if(typeof compare !== 'function') {
+    compare = (a, b) => {return a === b};
+  }
+  let missing = last_array.filter((elem) => {
+    return cur_array.every((new_elem) => { return !compare(elem, new_elem) });
+  });
+  return missing;
+}
+
+function trackerFactory({watchFunction}) {
+  if (typeof watchFunction !== 'function') {
+    return undefined;
+  }
+
+  let last_result = null;
+  let compare_function = null;
+  let element_left_cb = null;
+  let element_join_cb = null;
+
+  trackerMethods = {
+    setComparator: (comparator) => {
+      compare_function = comparator;
+      return trackerMethods;
+    },
+    on: (event_name, cb) => {
+      if (event_name === "element_left") {
+        element_left_cb = cb;
+      } else
+      if (event_name === "element_join") {
+        element_join_cb = cb;
+      }
+      return trackerMethods;
+    },
+    call: () => {
+      Promise.resolve(watchFunction())
+      .then((current_result) => {
+        if (last_result === null) {
+          last_result = current_result;
+          return trackerMethods;
+        }
+        let elementsThatLeft =
+          getMissingElements(last_result, current_result, compare_function);
+        let elementsThatJoined =
+          getMissingElements(current_result, last_result, compare_function);
+        if (elementsThatLeft.length > 0 && typeof element_left_cb === 'function') {
+          element_left_cb(elementsThatLeft);
+        }
+        if (elementsThatJoined.length > 0 && typeof element_join_cb === 'function') {
+          element_join_cb(elementsThatJoined);
+        }
+        last_result = current_result;
+      });
+    }
+  }
+  return trackerMethods;
+}
---
+++ tests/function_tracker.spec.js
@@ -0,0 +1,232 @@
+const chai = require('chai');
+const sinon = require('sinon');
+const sinonChai = require('sinon-chai');
+
+const trackerFactory = require('../index');
+
+chai.should();
+chai.use(sinonChai);
+
+function makeMockFunction(...values) {
+  let i = 0;
+  return () => {
+    i++;
+    return values[i-1];
+  }
+}
+
+function makePromiseMockFunction(...values) {
+  let i = 0;
+  return () => {
+    return new Promise((resolve, reject) => {
+      i++;
+      resolve(values[i-1]);
+    });
+  };
+}
+
+describe("function can tell the difference between changing return values", () => {
+  it("returns which elements have left the array", (done) => {
+    let elementLeaveMock = sinon.spy();
+    let tracker = trackerFactory({
+      watchFunction: makeMockFunction([1,2,3], [1,2])
+    });
+
+    tracker.on('element_left', elementLeaveMock);
+
+    tracker.call();
+    tracker.call();
+
+    Promise.resolve().then(() => {
+      elementLeaveMock.should.have.been.calledWith([3]);
+      done();
+    });
+  });
+
+  it("does not trigger the event when no elements leave", (done) => {
+    let elementLeaveMock = sinon.spy();
+    let tracker = trackerFactory({
+      watchFunction: makeMockFunction([1,2,3], [1,3,2,4])
+    });
+
+    tracker.on('element_left', elementLeaveMock);
+
+    tracker.call();
+    tracker.call();
+
+    Promise.resolve().then(() => {
+      elementLeaveMock.should.have.not.been.called;
+      done();
+    });
+  });
+
+  it("triggers an event when elements join", (done) => {
+    let elementJoinMock = sinon.spy();
+    let tracker = trackerFactory({
+      watchFunction: makeMockFunction([1,2], [1,2,3])
+    });
+
+    tracker.on('element_join', elementJoinMock);
+
+    tracker.call();
+    tracker.call();
+
+    Promise.resolve().then(() => {
+      elementJoinMock.should.have.been.calledWith([3]);
+      done();
+    });
+  });
+
+  it("does not trigger an event when elements don't join", (done) => {
+    let elementJoinMock = sinon.spy();
+    let tracker = trackerFactory({
+      watchFunction: makeMockFunction([1,2,3], [1,2,3])
+    });
+
+    tracker.on('element_join', elementJoinMock);
+
+    tracker.call();
+    tracker.call();
+
+    Promise.resolve().then(() => {
+      elementJoinMock.should.have.not.been.called;
+      done();
+    });
+  });
+
+  it("triggers an element_join event with custom comparator", (done) => {
+    let elementJoinMock = sinon.spy();
+    let tracker = trackerFactory({
+      watchFunction: makeMockFunction(
+        [{id: 1, name: "a"}],
+        [{id: 2, name: "b"}, {id: 1, name: "b"}])
+    });
+
+    tracker.on('element_join', elementJoinMock);
+    tracker.setComparator((a,b) => {return a.id === b.id});
+
+    tracker.call();
+    tracker.call();
+
+    Promise.resolve().then(() => {
+      elementJoinMock.should.have.been.calledWith([{id: 2, name: "b"}])
+      done();
+    });
+  });
+});
+
+describe("function can tell the difference between changing return promises", () => {
+  it("returns which elements have left the array", (done) => {
+    let elementLeaveMock = sinon.spy();
+    let tracker = trackerFactory({
+      watchFunction: makePromiseMockFunction([1,2,3], [1,2])
+    });
+
+    tracker.on('element_left', elementLeaveMock);
+
+    tracker.call();
+    tracker.call();
+
+    Promise.resolve().then(() => {
+      elementLeaveMock.should.have.been.calledWith([3]);
+      done();
+    });
+  });
+
+  it("does not trigger the event when no elements leave", (done) => {
+    let elementLeaveMock = sinon.spy();
+    let tracker = trackerFactory({
+      watchFunction: makePromiseMockFunction([1,2,3], [1,3,2,4])
+    });
+
+    tracker.on('element_left', elementLeaveMock);
+
+    tracker.call();
+    tracker.call();
+
+    Promise.resolve().then(() => {
+      elementLeaveMock.should.have.not.been.called;
+      done();
+    });
+  });
+
+  it("triggers an event when elements join", (done) => {
+    let elementJoinMock = sinon.spy();
+    let tracker = trackerFactory({
+      watchFunction: makePromiseMockFunction([1,2], [1,2,3])
+    });
+
+    tracker.on('element_join', elementJoinMock);
+
+    tracker.call();
+    tracker.call();
+
+    Promise.resolve().then(() => {
+      elementJoinMock.should.have.been.calledWith([3]);
+      done();
+    });
+  });
+
+  it("does not trigger an event when elements don't join", (done) => {
+    let elementJoinMock = sinon.spy();
+    let tracker = trackerFactory({
+      watchFunction: makePromiseMockFunction([1,2,3], [1,2,3])
+    });
+
+    tracker.on('element_join', elementJoinMock);
+
+    tracker.call();
+    tracker.call();
+
+    Promise.resolve().then(() => {
+      elementJoinMock.should.have.not.been.called;
+      done();
+    });
+  });
+
+  it("triggers an element_join event with custom comparator", (done) => {
+    let elementJoinMock = sinon.spy();
+    let tracker = trackerFactory({
+      watchFunction: makePromiseMockFunction(
+        [{id: 1, name: "a"}],
+        [{id: 2, name: "b"}, {id: 1, name: "b"}])
+    });
+
+    tracker.on('element_join', elementJoinMock);
+    tracker.setComparator((a,b) => {return a.id === b.id});
+
+    tracker.call();
+    tracker.call();
+
+    Promise.resolve().then(() => {
+      elementJoinMock.should.have.been.calledWith([{id: 2, name: "b"}])
+      done();
+    });
+  });
+});
+
+describe("full example with elements joining and leaving, initializing with chaining", () => {
+  it('performs all functions at once', (done) => {
+    let elementJoinMock = sinon.spy();
+    let elementLeaveMock = sinon.spy();
+
+    tracker = trackerFactory({
+      watchFunction: makePromiseMockFunction(
+        [{id: 1, name: "H"}, {id: 2, name: "G"}],
+        [{id: 1, name: "I"}, {id: 3, name: "Y"}]
+      )
+    })
+    .setComparator((a,b) => {return a.id === b.id})
+    .on('element_join', elementJoinMock)
+    .on('element_left', elementLeaveMock)
+
+    tracker.call()
+    tracker.call()
+
+    Promise.resolve().then(() => {
+      elementJoinMock.should.have.been.calledWith([{id: 3, name: "Y"}]);
+      elementLeaveMock.should.have.been.calledWith([{id: 2, name: "G"}]);
+      done();
+    })
+  })
+})
